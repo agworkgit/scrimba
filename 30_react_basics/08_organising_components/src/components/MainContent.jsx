4e11094b875c18cc1e1a9f777e9d0d74b2063561
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-05-07 19:41:42 +0100

    finished ReactFacts project

--- 30_react_basics/08_organising_components/src/components/MainContent.jsx
+++ 30_react_basics/08_organising_components/src/components/MainContent.jsx
@@ -2,17 +2,23 @@ export default function MainContent() {
     return (
         <main className='main-content'>
             <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It has a large community</li>
-                <li>It is regularly maintained and updated</li>
-                <li>It increases employability</li>
-                <li>It is adpots a declarative way of writing code</li>
-                <li>It can be used for both web and native development</li>
-                <li>
+            <ul className='fact-list'>
+                <li className='fact-item'>It has a large community</li>
+                <li className='fact-item'>
+                    It is regularly maintained and updated
+                </li>
+                <li className='fact-item'>It increases employability</li>
+                <li className='fact-item'>
+                    It is adpots a declarative way of writing code
+                </li>
+                <li className='fact-item'>
+                    It can be used for both web and native development
+                </li>
+                <li className='fact-item'>
                     It is composable and can make a larger project easy to work
                     on
                 </li>
-            </ol>
+            </ul>
         </main>
     );
 }
