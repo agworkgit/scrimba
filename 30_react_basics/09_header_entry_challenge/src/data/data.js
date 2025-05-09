85ba7b6b9b35b17cb3c9790bdedac0a6b3b60fcc
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-05-09 10:22:39 +0100

    fixed responsiveness

--- 30_react_basics/09_header_entry_challenge/src/data/data.js
+++ 30_react_basics/09_header_entry_challenge/src/data/data.js
@@ -2,7 +2,7 @@ export default [
     {
         id: 1,
         img: {
-            src: "https://scrimba.com/links/travel-journal-japan-image-url",
+            src: "https://images.unsplash.com/photo-1578271887552-5ac3a72752bc?q=80&w=3569&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             alt: "Mount Fuji"
         },
         title: "Mount Fuji",
@@ -14,7 +14,7 @@ export default [
     {
         id: 2,
         img: {
-            src: "https://scrimba.com/links/travel-journal-australia-image-url",
+            src: "https://images.unsplash.com/photo-1656177310138-9afa186d4a7b?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             alt: "Sydney Opera House"
         },
         title: "Sydney Opera House",
@@ -26,7 +26,7 @@ export default [
     {
         id: 3,
         img: {
-            src: "https://scrimba.com/links/travel-journal-norway-image-url",
+            src: "https://images.unsplash.com/photo-1601439678777-b2b3c56fa627?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             alt: "Geirangerfjord"
         },
         title: "Geirangerfjord",
@@ -35,4 +35,77 @@ export default [
         dates: "01 Oct, 2021 - 18 Nov, 2021",
         text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
     },
-];
+    {
+        id: 4,
+        img: {
+            src: "https://images.unsplash.com/photo-1604175287072-b5e71423060c?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Eiffel Tower"
+        },
+        title: "Eiffel Tower",
+        country: "France",
+        googleMapsLink: "https://maps.app.goo.gl/1DRQa9d5X2zAVvcg6",
+        dates: "10 Mar, 2022 - 20 Mar, 2022",
+        text: "The Eiffel Tower is one of the most recognizable structures in the world and a symbol of Paris and France."
+    },
+    {
+        id: 5,
+        img: {
+            src: "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Colosseum"
+        },
+        title: "Colosseum",
+        country: "Italy",
+        googleMapsLink: "https://maps.app.goo.gl/zkv5PtSycMgwTR9F6",
+        dates: "5 Apr, 2022 - 15 Apr, 2022",
+        text: "The Colosseum in Rome is a historic amphitheater and one of the greatest works of Roman architecture and engineering."
+    },
+    {
+        id: 6,
+        img: {
+            src: "https://images.unsplash.com/photo-1615551043360-33de8b5f410c?q=80&w=3576&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Grand Canyon"
+        },
+        title: "Grand Canyon",
+        country: "USA",
+        googleMapsLink: "https://maps.app.goo.gl/W7U6CU1fM1XDZy1p8",
+        dates: "22 Jun, 2022 - 30 Jun, 2022",
+        text: "The Grand Canyon is a steep-sided canyon carved by the Colorado River in Arizona, known for its visually overwhelming size and intricate landscape."
+    },
+    {
+        id: 7,
+        img: {
+            src: "https://images.unsplash.com/photo-1700677866588-95226be09b39?q=80&w=3774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Christ the Redeemer"
+        },
+        title: "Christ the Redeemer",
+        country: "Brazil",
+        googleMapsLink: "https://maps.app.goo.gl/PnGT7zsvtVcKvFG88",
+        dates: "8 Jul, 2022 - 15 Jul, 2022",
+        text: "Christ the Redeemer is an iconic statue of Jesus Christ in Rio de Janeiro, standing 30 meters tall atop the Corcovado mountain."
+    },
+    {
+        id: 8,
+        img: {
+            src: "https://images.unsplash.com/photo-1508804185872-d7badad00f7d?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Great Wall of China"
+        },
+        title: "Great Wall of China",
+        country: "China",
+        googleMapsLink: "https://maps.app.goo.gl/1v3RZV4sfVRC1KoZ7",
+        dates: "1 Sep, 2022 - 10 Sep, 2022",
+        text: "The Great Wall of China is a series of fortifications that stretch across northern China, built over centuries to protect against invasions."
+    },
+    {
+        id: 9,
+        img: {
+            src: "https://images.unsplash.com/photo-1553913861-c0fddf2619ee?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Pyramids of Giza"
+        },
+        title: "Pyramids of Giza",
+        country: "Egypt",
+        googleMapsLink: "https://maps.app.goo.gl/DnhkM6xAhbAbzBRs8",
+        dates: "14 Oct, 2022 - 22 Oct, 2022",
+        text: "The Pyramids of Giza are ancient pyramid structures near Cairo and the last of the original Seven Wonders of the Ancient World still in existence."
+    }
+]
+
